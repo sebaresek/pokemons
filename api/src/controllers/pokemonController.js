6f1051d8dc6b016822e6718066d4022a5ac76538
resekk <sebaresekk@gmail.com>
2023-05-21 17:45:13 -0300

    searchPokemonByName

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -122,7 +122,7 @@ const searchPokemonByName = async (name) => {
       .then(res => cleanObject(res.data));
   
     // Comprobamos si no se encontraron resultados en ambas fuentes de datos
-    if (apiPokemon === null && databasePokemons.length === 0) {
+    if (apiPokemon === null && databasePokemons === null) {
       throw Error(`El Pokémon con el nombre ${name} no existe`);
     }
   
