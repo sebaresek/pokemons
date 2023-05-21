ceff39be098b59d53a981b9f4cab63adfd9c6b7a
resekk <sebaresekk@gmail.com>
2023-05-21 12:41:57 -0300

    css addDelete

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -20,15 +20,17 @@ const  Card = (props) => {
 
     return (
         <div className={style.card}  >
-            <div className={style.font}>
-                <img src={props.image} alt={props.name} />
-            </div>
 
             {showDeleteButton && (
                 <div className={style.button}>
                     <button onClick={() => props.onDelete(props.id)}> DELETE </button>
                 </div>
             )}
+
+            <div className={style.font}>
+                <img src={props.image} alt={props.name} />
+            </div>
+
 
             <div className={style.back} >
                 <div>
